2b93683f28493210bab3652089f4c7703bed7691
ABEEGOLD <abeeutee@gmail.com>
2025-06-15 14:51:26 +0100

    java

---
+++ JAVA/PersonalityTest.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class PersonalityTest{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+	
+		ArrayList<String> questionnaire = new ArrayList<>();
+	
+	while(true){
+		System.out.print("Take your test: ");
+		String question1 = input.nextLine();
+		
+
+
+
+
+
+
+
+}
+
+	question1 = "I strive for accuracy and precision in all my tasks.";
+
+
+
+
+
+
+}
+
+}
---
+++ JAVA/StudentGrade.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class StudentGrade{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		
+	ArrayList<Integer> studentNumber = new ArrayList<>();
+	ArrayList<Integer> subjectNumber = new ArrayList<>();
+
+
+while(true){
+	System.pout.print("How many student do you have?: ");
+	int NumberOfStudent = input.nextInt(); 
+	studentNumber.add(NumberOfStudent);
+
+	System.out.print("How many subject do they offer?: ");
+	int numberOfSubject = input.nextInt();
+	subjectNumber.add(numberOfSubject);
+
+	int sudentCount = Student 1, Student 2, Student 3, Student 4;
+	int Student 1 = SUB1,SUB2,SUB3;
+
+	int TOT =   
+
+}
+	
+
+	System.out.print("\n======================================================\n");
+
+	System.out.print("STUDENT",  "SUB1",  "SUB2",  "SUB3",  "TOT",  "AVE",  "POS");
+		System.out.print("\n======================================================\n");
+	System.out.print("Student 1");
+	System.out.print("Student 2");
+	System.out.print("Student 3");
+	System.out.print("Student 4");
+
+
+
+for (int i = 0; i < studentNumber.size(); i++) {
+        	int totalStudent = subjectNumber.get(i) * amountPerUnit.get(i);
+            	System.out.printf("%-20s %10d %10.2f %15.2f%n",itemBought.get(i), numOfProduct.get(i), amountPerUnit.get(i), totalItemPrice);
+        }
+
+
+
+
+
+
+
+
+
+	}
+
+}

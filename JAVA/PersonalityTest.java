28704f464758d390cbb8f30d71446d37340be751
ABEEGOLD <abeeutee@gmail.com>
2025-06-16 16:03:02 +0100

    update

--- JAVA/PersonalityTest.java
+++ JAVA/PersonalityTest.java
@@ -5,196 +5,252 @@ public class PersonalityTest{
 	
 	
 		System.out.print("Take your test: \n");
-		String questions = scanner.nextLine();
-		
-
+		String answers = scanner.nextLine();
+		System.out.print("What is your name:");
+		String Name = scanner.nextLine();
+while(true){
         System.out.println("a) Hanging out with friends");
         System.out.println("b) Reading a book");
         String answer1 = scanner.nextLine();
-        if (answer1.equalsIgnoreCase("a")) score += 2;
-        else if (answer1.equalsIgnoreCase("b")) score += 1;
-        else if (answer1.equalsIgnoreCase("c")) score += 3;
-
-
+        if (answer1.equalsIgnoreCase("a")){
+        }else if (answer1.equalsIgnoreCase("b")){
+	}else{
+		System.out.println("Invalid");
+		break;
+	}
+	
 
         System.out.println("a) Enjoy group work");
         System.out.println("b) Enjoy working alone");
-        String answer1 = scanner.nextLine();
-        if (answer1.equalsIgnoreCase("a")) score += 2;
-        else if (answer1.equalsIgnoreCase("b")) score += 1;
-        else if (answer1.equalsIgnoreCase("c")) score += 3;
+        String answer2 = scanner.nextLine();
+        if (answer2.equalsIgnoreCase("a")){
+       }else if (answer2.equalsIgnoreCase("b")){
+	}else{
+		System.out.println("Invalid");
+		break;
+	}
 
 
 
         System.out.println("a) Questions everything");
         System.out.println("b) keep to self");
-        String answer1 = scanner.nextLine();
-        if (answer1.equalsIgnoreCase("a")) score += 2;
-        else if (answer1.equalsIgnoreCase("b")) score += 1;
-        else if (answer1.equalsIgnoreCase("c")) score += 3;
+        String answer3 = scanner.nextLine();
+        if (answer3.equalsIgnoreCase("a")){
+        }else if (answer3.equalsIgnoreCase("b")){
+	}else{
+		System.out.println("Invalid");
+		break;
+	}
 
 
 
         System.out.println("a) Logical about happentance");
         System.out.println("b) Emotional");
-        String answer1 = scanner.nextLine();
-        if (answer1.equalsIgnoreCase("a")) score += 2;
-        else if (answer1.equalsIgnoreCase("b")) score += 1;
-        else if (answer1.equalsIgnoreCase("c")) score += 3;
+        String answer4 = scanner.nextLine();
+        if (answer4.equalsIgnoreCase("a")){
+        }else if (answer4.equalsIgnoreCase("b")){
+	}else{
+		System.out.println("Invalid");
+		break;
+	}
 
 
 
         System.out.println("a) Out going person");
         System.out.println("b) Reserved");
-        String answer1 = scanner.nextLine();
-        if (answer1.equalsIgnoreCase("a")) score += 2;
-        else if (answer1.equalsIgnoreCase("b")) score += 1;
-        else if (answer1.equalsIgnoreCase("c")) score += 3;
+        String answer5 = scanner.nextLine();
+        if (answer5.equalsIgnoreCase("a")){
+        }else if (answer5.equalsIgnoreCase("b")){
+	}else{
+		System.out.println("Invalid");
+		break;
+	}
 
 
 
         System.out.println("a) More of a practical person");
         System.out.println("b) More of theoretical");
-        String answer1 = scanner.nextLine();
-        if (answer1.equalsIgnoreCase("a")) score += 2;
-        else if (answer1.equalsIgnoreCase("b")) score += 1;
-        else if (answer1.equalsIgnoreCase("c")) score += 3;
+        String answer6 = scanner.nextLine();
+        if (answer6.equalsIgnoreCase("a")){
+        }else if (answer6.equalsIgnoreCase("b")){
+	}else{
+		System.out.println("Invalid");
+		break;
+	}
 
 
 
         System.out.println("a) Organized");
         System.out.println("b) Flexible");
-        String answer1 = scanner.nextLine();
-        if (answer1.equalsIgnoreCase("a")) score += 2;
-        else if (answer1.equalsIgnoreCase("b")) score += 1;
-        else if (answer1.equalsIgnoreCase("c")) score += 3;
+        String answer7 = scanner.nextLine();
+        if (answer7.equalsIgnoreCase("a")){
+        }else if (answer7.equalsIgnoreCase("b")){
+	}else{
+		System.out.println("Invalid");
+		break;
+	}
 
 
 
         System.out.println("a) unplanned");
         System.out.println("b) Spontaneous");
-        String answer1 = scanner.nextLine();
-        if (answer1.equalsIgnoreCase("a")) score += 2;
-        else if (answer1.equalsIgnoreCase("b")) score += 1;
-        else if (answer1.equalsIgnoreCase("c")) score += 3;
+        String answer8 = scanner.nextLine();
+        if (answer8.equalsIgnoreCase("a")){
+        }else if (answer8.equalsIgnoreCase("b")){
+	}else{
+		System.out.println("Invalid");
+		break;
+	}
 
 
         System.out.println("a) Control");
         System.out.println("b) Govern");
-        String answer1 = scanner.nextLine();
-        if (answer1.equalsIgnoreCase("a")) score += 2;
-        else if (answer1.equalsIgnoreCase("b")) score += 1;
-        else if (answer1.equalsIgnoreCase("c")) score += 3;
+        String answer9 = scanner.nextLine();
+        if (answer9.equalsIgnoreCase("a")){
+        }else if (answer9.equalsIgnoreCase("b")){
+	}else{
+		System.out.println("Invalid");
+		break;
+	}
 
 
 
         System.out.println("a) intiate action");
         System.out.println("b) Reflect action");
-        String answer1 = scanner.nextLine();
-        if (answer1.equalsIgnoreCase("a")) score += 2;
-        else if (answer1.equalsIgnoreCase("b")) score += 1;
-        else if (answer1.equalsIgnoreCase("c")) score += 3;
+        String answer10 = scanner.nextLine();
+        if (answer10.equalsIgnoreCase("a")){
+        }else if (answer10.equalsIgnoreCase("b")){
+	}else{
+		System.out.println("Invalid");
+		break;
+	}
 
 
 
         System.out.println("a) Plan ahead");
         System.out.println("b) Go with flow");
-        String answer1 = scanner.nextLine();
-        if (answer1.equalsIgnoreCase("a")) score += 2;
-        else if (answer1.equalsIgnoreCase("b")) score += 1;
-        else if (answer1.equalsIgnoreCase("c")) score += 3;
+        String answer11 = scanner.nextLine();
+        if (answer11.equalsIgnoreCase("a")){
+        }else if (answer11.equalsIgnoreCase("b")){
+	}else{
+		System.out.println("Invalid");
+		break;
+	}
 
 
 
         System.out.println("a) facts");
         System.out.println("b) Philosophical");
-        String answer1 = scanner.nextLine();
-        if (answer1.equalsIgnoreCase("a")) score += 2;
-        else if (answer1.equalsIgnoreCase("b")) score += 1;
-        else if (answer1.equalsIgnoreCase("c")) score += 3;
+        String answer12 = scanner.nextLine();
+        if (answer12.equalsIgnoreCase("a")){
+        }else if (answer12.equalsIgnoreCase("b")){
+	}else{
+		System.out.println("Invalid");
+		break;
+	}
 
 
 
         System.out.println("a) Focus on the now");
         System.out.println("b) Look to the future");
-        String answer1 = scanner.nextLine();
-        if (answer1.equalsIgnoreCase("a")) score += 2;
-        else if (answer1.equalsIgnoreCase("b")) score += 1;
-        else if (answer1.equalsIgnoreCase("c")) score += 3;
+        String answer13 = scanner.nextLine();
+        if (answer13.equalsIgnoreCase("a")){
+        }else if (answer13.equalsIgnoreCase("b")){
+	}else{
+		System.out.println("Invalid");
+		break;
+	}
 
 
 
         System.out.println("a) Criticize");
         System.out.println("b) Appreciative");
-        String answer1 = scanner.nextLine();
-        if (answer1.equalsIgnoreCase("a")) score += 2;
-        else if (answer1.equalsIgnoreCase("b")) score += 1;
-        else if (answer1.equalsIgnoreCase("c")) score += 3;
+        String answer14 = scanner.nextLine();
+        if (answer14.equalsIgnoreCase("a")){
+        }else if (answer14.equalsIgnoreCase("b")){
+	}else{
+		System.out.println("Invalid");
+		break;
+	}
 
 
 
         System.out.println("a) Accommodating");
         System.out.println("b) Not a people person");
-        String answer1 = scanner.nextLine();
-        if (answer1.equalsIgnoreCase("a")) score += 2;
-        else if (answer1.equalsIgnoreCase("b")) score += 1;
-        else if (answer1.equalsIgnoreCase("c")) score += 3;
+        String answer15 = scanner.nextLine();
+        if (answer15.equalsIgnoreCase("a")){
+        }else if (answer15.equalsIgnoreCase("b")){
+	}else{
+		System.out.println("Invalid");
+		break;
+	}
 
 
 
         System.out.println("a) Good sense of humor");
         System.out.println("b) Recentive");
-        String answer1 = scanner.nextLine();
-        if (answer1.equalsIgnoreCase("a")) score += 2;
-        else if (answer1.equalsIgnoreCase("b")) score += 1;
-        else if (answer1.equalsIgnoreCase("c")) score += 3;
+        String answer16 = scanner.nextLine();
+        if (answer16.equalsIgnoreCase("a")){
+        }else if (answer16.equalsIgnoreCase("b")){
+	}else{
+		System.out.println("Invalid");
+		break;
+	}
 
 
 
         System.out.println("a) Seek private");
         System.out.println("b) Interact with others");
-        String answer1 = scanner.nextLine();
-        if (answer1.equalsIgnoreCase("a")) score += 2;
-        else if (answer1.equalsIgnoreCase("b")) score += 1;
-        else if (answer1.equalsIgnoreCase("c")) score += 3;
+        String answer17 = scanner.nextLine();
+        if (answer17.equalsIgnoreCase("a")){
+        }else if (answer17.equalsIgnoreCase("b")){
+	}else{
+		System.out.println("Invalid");
+		break;
+	}
 
 
 
         System.out.println("a) Structured");
         System.out.println("b) Easy going");
-        String answer1 = scanner.nextLine();
-        if (answer1.equalsIgnoreCase("a")) score += 2;
-        else if (answer1.equalsIgnoreCase("b")) score += 1;
-        else if (answer1.equalsIgnoreCase("c")) score += 3;
+        String answer18 = scanner.nextLine();
+        if (answer18.equalsIgnoreCase("a")){ 
+        }else if (answer18.equalsIgnoreCase("b")){
+	}else{
+		System.out.println("Invalid");
+		break;
+	}
 
 
 
         System.out.println("a) Work out ideas");
         System.out.println("b) Dreamer");
-        String answer1 = scanner.nextLine();
-        if (answer1.equalsIgnoreCase("a")) score += 2;
-        else if (answer1.equalsIgnoreCase("b")) score += 1;
-        else if (answer1.equalsIgnoreCase("c")) score += 3;
-
+        String answer19 = scanner.nextLine();
+        if (answer19.equalsIgnoreCase("a")){
+        }else if (answer19.equalsIgnoreCase("b")){ 
+	}else{
+		System.out.println("Invalid");
+		break;
+	}
 
 
         System.out.println("a) Communictive");
         System.out.println("b) Internal");
-        String answer1 = scanner.nextLine();
-        if (answer1.equalsIgnoreCase("a")) score += 2;
-        else if (answer1.equalsIgnoreCase("b")) score += 1;
-        else if (answer1.equalsIgnoreCase("c")) score += 3;
+        String answer20 = scanner.nextLine();
+        if (answer20.equalsIgnoreCase("a")){
+        }else if (answer20.equalsIgnoreCase("b")){
+	}else{
+		System.out.println("Invalid");
+		break;
+	}
 
 
-
-        System.out.println("a)Tough-minded");
-        System.out.println("b) Merciful");
-        String answer1 = scanner.nextLine();
-        if (answer1.equalsIgnoreCase("a")) score += 2;
-        else if (answer1.equalsIgnoreCase("b")) score += 1;
-        else if (answer1.equalsIgnoreCase("c")) score += 3;
-
-
+	String question = scanner.nextLine();
+	scanner.nextLine();
+	if(question != "20"){
+		System.out.print("Abobi e no dey");
+		break;
+  }      	}
 
 
 
